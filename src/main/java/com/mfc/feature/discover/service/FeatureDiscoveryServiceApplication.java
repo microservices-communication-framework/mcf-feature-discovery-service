81377994f4af83336a3520e1fef629cd995ae42f
Ahmed Riahi <contact@ahmedriahi.net>
2022-11-26 21:43:17 +0100

    first commit

---
+++ src/main/java/com/mfc/feature/discover/service/FeatureDiscoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mfc.feature.discover.service;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeatureDiscoveryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeatureDiscoveryServiceApplication.class);
+    }
+}
